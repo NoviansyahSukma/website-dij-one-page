df5c5d003030f58c2980e5f7ed1351c51776841f
Noviansyah <cymbalavedis@gmail.com>
2025-10-21 16:24:58 +0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/website-dij/",
+  base: "/website-dij-one-page/",
 });
